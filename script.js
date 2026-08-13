6f9977cd0fb0bea61cafce2140968724679c3a04
ndidisgit <n.ndidi@gmail.com>
2026-08-13 08:21:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,5 @@ document.querySelectorAll('.nav a').forEach(link => {
 });
 
 window.onload = function() {
-   // Reset the form fields when the page loads
    document.getElementById("form").reset();
 };
